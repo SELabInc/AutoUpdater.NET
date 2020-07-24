690ad71bf71ddfd9b1d90af016f609c8857e05cc
박태형 <thpark@selab.co.kr>
2020-07-24 15:05:22 +0900

    업데이트 리스트 추가 후 다운로드 방식 제거

--- AutoUpdater.NET/DownloadUpdateDialog.cs
+++ AutoUpdater.NET/DownloadUpdateDialog.cs
@@ -50,10 +50,10 @@ namespace AutoUpdaterDotNET
         {
             DownloadSet();
             string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36";
-            _updateList.Add(new FileModel()
-            {
-                Name = "UpdateList.xml"
-            });
+            //_updateList.Add(new FileModel()
+            //{
+            //    Name = "UpdateList.xml"
+            //});
 
             downloadMaxCount = _updateList.Count;
 
